d12467cb98df4f751cc1a58c4f3c5e6da11eca0d
shuopeng yin <shuopengyin@css-MacBook-Pro-3.local>
2016-02-13 10:57:35 -0800

    First commit, starter code

---
+++ ActiveTran/Student.swift
@@ -0,0 +1,66 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import Foundation
+
+struct Student {
+  
+  let key: String!
+  let name: String!
+  let parentID : String!
+  let staffID: String!
+  let school: String!
+  let ref: Firebase?
+  var arrived: Bool!
+  
+  // Initialize from arbitrary data
+  init(name: String, school: String, arrived: Bool, key: String = "", parentID : String!, staffID : String!) {
+    self.key = key
+    self.name = name
+    self.school = school
+    self.arrived = arrived
+    self.parentID = parentID;
+    self.staffID = staffID;
+    self.ref = nil
+  }
+  
+  init(snapshot: FDataSnapshot) {
+    key = snapshot.key
+    name = snapshot.value["name"] as! String
+    school = snapshot.value["school"] as! String
+    arrived = snapshot.value["arrived"] as! Bool
+    parentID = snapshot.value["parentID"] as! String
+    staffID = snapshot.value["staffID"] as! String
+    ref = snapshot.ref
+  }
+  
+  func toAnyObject() -> AnyObject {
+    return [
+      "name": name,
+      "school": school,
+      "arrived": arrived,
+      "parentID": parentID,
+      "staffID": staffID
+    ]
+  }
+  
+}
---
+++ ActiveTran/StudentListTableViewController.swift
@@ -0,0 +1,183 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+
+class StudentListTableViewController: UITableViewController {
+
+  // MARK: Constants
+  let ListToUsers = "ListToUsers"
+  
+  // MARK: Properties 
+  var students = [Student]()
+  var user: User!
+  var userCountBarButtonItem: UIBarButtonItem!
+  let ref = Firebase(url: "https://activetransportation.firebaseio.com/students")
+  
+  // MARK: UIViewController Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    // Set up swipe to delete
+    tableView.allowsMultipleSelectionDuringEditing = false
+    
+    // User Count
+    userCountBarButtonItem = UIBarButtonItem(title: "1", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("userCountButtonDidTouch"))
+    userCountBarButtonItem.tintColor = UIColor.whiteColor()
+    navigationItem.leftBarButtonItem = userCountBarButtonItem
+    
+    user = User(uid: "FakeId", email: "hungry@person.food")
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+      super.viewDidAppear(animated)
+        
+        // 1
+      ref.observeEventType(.Value, withBlock: { snapshot in
+            
+            // 2
+            var newStudents = [Student]()
+            
+            // 3
+            for item in snapshot.children {
+                
+                // 4
+                let newStudent = Student(snapshot: item as! FDataSnapshot)
+                newStudents.append(newStudent)
+            }
+            
+            // 5
+            self.students = newStudents
+            self.tableView.reloadData()
+        })
+    }
+    
+  override func viewDidDisappear(animated: Bool) {
+    super.viewDidDisappear(animated)
+    
+  }
+  
+  // MARK: UITableView Delegate methods
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return students.count
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("ItemCell") as! UITableViewCell!
+    let studentSelected = students[indexPath.row]
+    
+    cell.textLabel?.text = studentSelected.name
+    cell.detailTextLabel?.text = studentSelected.parentID
+    
+    // Determine whether the cell is checked
+    toggleCellCheckbox(cell, isCompleted: studentSelected.arrived)
+    
+    return cell
+  }
+  
+  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    return true
+  }
+  
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // 1
+            let student = students[indexPath.row]
+            // 2
+            student.ref?.removeValue()
+        }
+    }
+  
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // 1
+        let cell = tableView.cellForRowAtIndexPath(indexPath)!
+        // 2
+        var studentSelected = students[indexPath.row]
+        // 3
+        let toggledCompletion = !studentSelected.arrived
+        // 4
+        toggleCellCheckbox(cell, isCompleted: toggledCompletion)
+        // 5
+        studentSelected.ref?.updateChildValues([
+            "arrived": toggledCompletion
+            ])
+    }
+    
+  func toggleCellCheckbox(cell: UITableViewCell, isCompleted: Bool) {
+    if !isCompleted {
+      cell.accessoryType = UITableViewCellAccessoryType.None
+      cell.textLabel?.textColor = UIColor.blackColor()
+      cell.detailTextLabel?.textColor = UIColor.blackColor()
+    } else {
+      cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+      cell.textLabel?.textColor = UIColor.grayColor()
+      cell.detailTextLabel?.textColor = UIColor.grayColor()
+    }
+  }
+  
+  // MARK: Add Item
+  
+  @IBAction func addButtonDidTouch(sender: AnyObject) {
+    // Alert View for input
+    var alert = UIAlertController(title: "Student",
+      message: "Add a student",
+      preferredStyle: .Alert)
+    
+    let saveAction = UIAlertAction(title: "Save",
+        style: .Default) { (action: UIAlertAction!) -> Void in
+            
+            // 1
+            let textField = alert.textFields![0] as UITextField
+            
+            // 2
+            let student = Student(name: textField.text!, school: "", arrived: false,  parentID: self.user.email, staffID: "" )
+            
+            // 3
+            let studentRef = self.ref.childByAppendingPath(textField.text!.lowercaseString)
+            
+            // 4
+            studentRef.setValue(student.toAnyObject())
+    }
+    let cancelAction = UIAlertAction(title: "Cancel",
+      style: .Default) { (action: UIAlertAction!) -> Void in
+    }
+    
+    alert.addTextFieldWithConfigurationHandler {
+      (textField: UITextField!) -> Void in
+    }
+    
+    alert.addAction(saveAction)
+    alert.addAction(cancelAction)
+    
+    presentViewController(alert,
+      animated: true,
+      completion: nil)
+  }
+  
+  func userCountButtonDidTouch() {
+    performSegueWithIdentifier(ListToUsers, sender: nil)
+  }
+  
+}
