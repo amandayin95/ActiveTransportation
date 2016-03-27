14d4c9cbe62521ff3ce15046bf602bdb305b8618
shuopeng yin <shuopengyin@WL-197-224.WPA.Claremont.Edu>
2016-03-26 19:53:50 -0700

    adding phone call and text options

--- ActiveTran/ContactInfoViewController.swift
+++ ActiveTran/ContactInfoViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import MessageUI
 
 /*
  * ContactInfoViewController: Controller for detailed info on parents' contact information.
@@ -7,7 +8,7 @@ import UIKit
  * Connects to Firebase to query parents' contact info.
  */
 
-class ContactInfoViewController: UITableViewController {
+class ContactInfoViewController: UITableViewController, MFMessageComposeViewControllerDelegate {
     
     // MARK: Property passed in through segue
     var studentSelected:Student!
@@ -66,4 +67,130 @@ class ContactInfoViewController: UITableViewController {
     }
     
     
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Find the cell that user tapped using cellForRowAtIndexPath
+        let cell = tableView.cellForRowAtIndexPath(indexPath)!
+        // Get the corresponding GreoceryItem by using the index path's row
+        var userSelected = users[indexPath.row]
+        
+        
+        var alertTitle = "Contact \(userSelected.name.capitalizedString)"
+        var alertMessage = "How do you want to contact \(userSelected.contactInfo)?"
+        
+        // show an alert and ask whether to call the parent/staff or not
+        let alert = UIAlertController(title: alertTitle,
+            message: alertMessage ,
+            preferredStyle: .Alert)
+        
+        let callAction = UIAlertAction(title: "Make a Call",
+            style: .Default) { (action: UIAlertAction) -> Void in
+                self.operation(userSelected.contactInfo, operation: "Call")
+        }
+        
+        let messageAction = UIAlertAction(title: "Send Text Message",
+            style: .Default) { (action: UIAlertAction) -> Void in
+                self.operation(userSelected.contactInfo, operation: "Text")
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction) -> Void in
+        }
+        
+        alert.addAction(callAction)
+        alert.addAction(messageAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+            animated: true,
+            completion: nil)
+    }
+    
+    private func operation(phoneNumber:String, operation:String) {
+        // check if the phone number string is valid
+        var valid = true
+        for c in phoneNumber.characters{
+            if (!(c >= "0" && c <= "9")) {
+                valid = false
+                break
+            }
+        }
+        
+        if (valid){
+            if (operation == "Call"){
+                let phoneCallURL:NSURL = NSURL(string: "tel://\(phoneNumber)")!
+                let application:UIApplication = UIApplication.sharedApplication()
+                if (application.canOpenURL(phoneCallURL)) {
+                    application.openURL(phoneCallURL);
+                }
+            }else if (operation == "Text"){
+                let alert = UIAlertController(title: "Sending Text Message to \(phoneNumber)",
+                    message: "Please enter the text message content below." ,
+                    preferredStyle: .Alert)
+                
+                let sendAction = UIAlertAction(title: "Send",
+                    style: .Default) { (action: UIAlertAction) -> Void in
+                        // Get the text field from the alert controller
+                        let textField = alert.textFields![0] as! UITextField
+                        
+                        if MFMessageComposeViewController.canSendText(){
+                            let msg:MFMessageComposeViewController=MFMessageComposeViewController()
+                            msg.recipients=[phoneNumber]
+                            msg.body=textField.text
+                            msg.messageComposeDelegate = self
+                            self.presentViewController(msg,animated:true,completion:nil)
+                            
+                        }
+                }
+                        
+                
+                let cancelAction = UIAlertAction(title: "Cancel",
+                    style: .Default) { (action: UIAlertAction) -> Void in
+                }
+
+                
+                alert.addTextFieldWithConfigurationHandler {
+                    (textField: UITextField!) -> Void in
+                }
+
+                
+                alert.addAction(sendAction)
+                alert.addAction(cancelAction)
+                
+                presentViewController(alert,
+                    animated: true,
+                    completion: nil)
+
+            }
+        }else{
+            let alert = UIAlertController(title: "Error",
+                message: "The contact information is not valid, please check with system administrator." ,
+                preferredStyle: .Alert)
+            
+            let okAction = UIAlertAction(title: "OK",
+                style: .Default) { (action: UIAlertAction) -> Void in
+            }
+            
+            alert.addAction(okAction)
+            
+            presentViewController(alert,
+                animated: true,
+                completion: nil)
+
+        }
+    }
+    
+    
+    func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult){
+        switch result.rawValue {
+        case MessageComposeResultCancelled.rawValue:
+            controller.dismissViewControllerAnimated(true, completion: nil)
+        case MessageComposeResultFailed.rawValue:
+            controller.dismissViewControllerAnimated(true, completion: nil)
+        case MessageComposeResultSent.rawValue:
+            controller.dismissViewControllerAnimated(false, completion: nil)
+            
+        default:
+            break
+        }
+}
 }
